805f849a898132eea776c897520a3bc99a1bc2aa
Afroja Anne <afrojaanne564@gmail.com>
2026-05-08 01:06:59 +0600

    Day 4: Product create feature added

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Add Product</h2>
+
+    <form method="POST" action="/products">
+        @csrf
+
+        <input type="text" name="name" placeholder="Product Name">
+        <br><br>
+
+        <textarea name="description" placeholder="Description"></textarea>
+        <br><br>
+
+        <input type="number" name="price" placeholder="Price">
+        <br><br>
+
+        <button type="submit">Save Product</button>
+    </form>
+</body>
+</html>
